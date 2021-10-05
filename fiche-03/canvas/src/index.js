fe46b1dff4f171e1422fc5f25bcb3094cdaf8867
Joachim Bastin <joachim.bastin@student.vinci.be>
2021-10-05 09:31:07 +0200

    add fiche-03 ex 1.1

--- fiche-03/canvas/src/index.js
+++ fiche-03/canvas/src/index.js
@@ -14,6 +14,33 @@ let pageHeight = page.clientHeight;
 myCanvas.width = pageWidth - 20;
 myCanvas.height = pageHeight;
 
+//Set square properties
+const INCREMENT = 5;
+let squareWidth = 20;
+let squareHeight = 20;
+let color = "blue";
+
+// animation on/off
+let animation = true;
+// event listener sur click gauche
+window.addEventListener("click", () => { // "click" ne concerne QUE click gauche
+  animation = (!animation);
+  if (animation == true)
+    requestAnimationFrame(drawOneFrame);
+});
+// event listener sur click droit
+window.addEventListener("contextmenu", event => { // "contextmenu" ne concerne QUE click droit
+  event.preventDefault(); // empêche le menu (sauvegarder l'iùmage sous...)
+  color = 'rgba('+Math.random()*256+','+Math.random()*256+','+Math.random()*256+',0.5)';
+});
+// event listener sur +/-  =>  attention au cas négatif
+window.addEventListener("keypress", event => {
+  if (event.key == "+")
+    squareWidth = squareHeight += INCREMENT;
+  if (event.key == "-")
+    squareWidth = squareHeight -= INCREMENT;
+});
+
 // call the callback to draw our animation when the browser is ready
 requestAnimationFrame(drawOneFrame);
 
@@ -23,20 +50,22 @@ function drawOneFrame() {
   // however that would not be optimized.
   myContext.clearRect(0, 0, pageWidth, pageHeight);
 
-  myContext.fillStyle = "blue"; //'rgba(255,0,0,0.5)';//'blue';
+  myContext.fillStyle = color; //'rgba(255,0,0,0.5)';//'blue';
 
   //draw dynamically the rectangles at random locations
   for (let i = 0; i < RECT_NUMBER; i++) {
     myContext.fillRect(
       Math.floor(Math.random() * pageWidth),
       Math.floor(Math.random() * pageHeight),
-      20,
-      20
+      squareWidth,
+      squareHeight
     );
   }
   // Refresh automatically the animation via this recursive call :
-  requestAnimationFrame(drawOneFrame);
+  if (animation)
+    requestAnimationFrame(drawOneFrame);
 
   // Slow the animation down via setTimeout
   //requestAnimationFrame(() => setTimeout(drawOneFrame,1000));
 }
+
